5691bd2973c30e64624df7eac76c321c57d0ad68
shyeom1023 <shyeom1023@gmail.com>
2024-07-22 01:48:10 +0900

    [coding-test] 푸드 파이터 대회

--- modules/code-test/src/main/kotlin/com/example/blog/level1/0007_푸드 파이터 대회.kt
+++ modules/code-test/src/main/kotlin/com/example/blog/level1/0007_푸드 파이터 대회.kt
@@ -13,6 +13,24 @@ private class Solution7 {
 
     fun solution(food: IntArray): String {
         var answer: String = ""
+
+        val result = mutableListOf<Int>()
+        for (i in 1..food.size) {
+            if (food.size == i) {
+                result.add(0)
+            } else {
+                val loop = food[i] / 2
+                for (j in 0 until loop) {
+                    result.add(i)
+                }
+            }
+        }
+
+        for (i in result.size - 2 downTo 0) {
+            result.add(result[i])
+        }
+
+        answer = result.joinToString("", "")
         return answer
     }
 }
