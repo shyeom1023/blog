c1b5eb4db231e9f14680731d0ce8eabe02851bd2
shyeom1023 <shyeom1023@gmail.com>
2024-07-21 23:45:03 +0900

    [coding-test] 푸드 파이터 대회 init

---
+++ modules/code-test/src/main/kotlin/com/example/blog/level1/0007_푸드 파이터 대회.kt
@@ -0,0 +1,18 @@
+package com.example.blog.level1
+
+fun main(args: Array<String>) {
+    val food: IntArray = intArrayOf(1, 3, 4, 6)
+    val result = Solution7().solution(food)
+
+    println("result : $result")
+
+//    result.forEach { println(it) }
+}
+
+private class Solution7 {
+
+    fun solution(food: IntArray): String {
+        var answer: String = ""
+        return answer
+    }
+}
